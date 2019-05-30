7d4e66c6ff9dc924d2368fe309faa2feab6aa53f
xvrzhao <xvrzhao@gmail.com>
2019-05-30 22:43:20 +0800

    fix the alphabet error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'I', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 String.prototype.getOrdinalNumberInAlphabet = function () {
     return alphabet.indexOf(this.toUpperCase()) + 1
